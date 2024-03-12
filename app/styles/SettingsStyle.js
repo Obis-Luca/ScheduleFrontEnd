e2f88064fe32fab94b3bfdcb9468e1ec17a86c8e
Florin1616 <115114026+Florin1616@users.noreply.github.com>
2024-03-12 11:38:46 +0200

    am facut pagina de choose si am adaugat buton de dark theme in settings

---
+++ app/styles/SettingsStyle.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+// import {white} from "react-native-paper/lib/typescript/styles/themes/v2/colors";
+const darkMode = StyleSheet.create({
+        bigView: {
+            backgroundColor: 'black',
+            marginTop: 50 ,
+            flexDirection: 'column'
+        },
+}
+
+)
+
+const lightMode = StyleSheet.create({
+    bigView: {
+        backgroundColor: 'white',
+        marginTop: 50 ,
+        flexDirection: 'column'
+    },
+})
+export { darkMode, lightMode };
